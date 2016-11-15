98b6aa5a70bfa8e2a0d1ce838bb6d174b98b37bf
Adam Baker and Bill Klinkow <bill@>
2016-11-15 09:10:22 -0800

    Finalized specs

---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,19 @@
+require('scrabble.rb')
+
+describe("String#scrabble") do
+  it("Returns a scrabble score for a letter (upper-case)") do
+    expect("A".scrabble()).to(eq(1))
+  end
+end
+
+describe("String#scrabble") do
+  it("Returns a scrabble score for a letter (lower-case)") do
+    expect("a".scrabble()).to(eq(1))
+  end
+end
+
+describe("String#scrabble") do
+  it("Returns a scrabble score for a word (mixed case)") do
+    expect("word".scrabble()).to(eq(8))
+  end
+end
--- spec/spec.rb
+++
@@ -1,3 +0,0 @@
-it("returns a scrabble score for a letter") do
-  expect("a".scrabble()).to(eq(1))
-end
